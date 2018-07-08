9707db25aeac0e6dcb1af61b808b67e560fb55d5
叶溪 <yexi@192.168.0.105>
2018-07-08 20:10:09 +0800

    Initial Commit

---
+++ first_c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  first_c
+//
+//  Created by 叶溪 on 2018/7/8.
+//  Copyright © 2018年 叶溪. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
